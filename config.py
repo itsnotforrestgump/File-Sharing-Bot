06e5155bf3a6ad1a6f6ade47b113056d0a19974f
itsnotforrestgump <97327135+itsnotforrestgump@users.noreply.github.com>
2022-01-08 10:18:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,30 +3,29 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = "5013844340:AAH95Z_E8bwChHMlklk5gX4l2r7xedsTBoM"
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = 735218
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = "5a401f985e2f2490685f5c4cefa77722"
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-
+CHANNEL_ID = -1001772884676
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = 831054990
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = "postgres://eimvpxiansqfsf:176607a160b6edd979dccda7392415361011f4141d78ec8b145c227af812ab7f@ec2-52-44-58-234.compute-1.amazonaws.com:5432/dbijimad8gm9g"
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = 0
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = 4
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = "Hello {mention}\n\nI can store private files in Specified Channel and other users can access it from special link."
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -35,19 +34,15 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = "Hello {mention}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+CUSTOM_CAPTION = "{previouscaption}"
 
 #Set true if you want Disable your Channel Posts Share button
-if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
-    DISABLE_CHANNEL_BUTTON = True
-else:
-    DISABLE_CHANNEL_BUTTON = False
+DISABLE_CHANNEL_BUTTON = False
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
